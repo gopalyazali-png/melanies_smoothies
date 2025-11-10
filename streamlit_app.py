a340aaf5d39dadb8d0327eea4586373d85ca72ad
gopalyazali-png <gopalyazali@gmail.com>
2025-11-10 10:29:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ if ingreditients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
-  import requests
-  smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-  #st.text(smoothiefroot_response)
-  sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=true)
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response)
+sf_df=st.dataframe(data=smoothiefroot_response.json(),use_container_width=true)
